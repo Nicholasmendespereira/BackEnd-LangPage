4b4e800f3c3df19c3db0b1d1717d528f6276f022
Nicholas mendes <98132580+Nicholasmendespereira@users.noreply.github.com>
2023-05-01 16:24:45 -0300

    conect-dataBase
    Neste commit, realizei a conecção com o banco de dados, postgreeSQL;

--- index.js
+++ index.js
@@ -1,31 +1,75 @@
-const express = require('express')
-const cors = require('cors')
+const { PrismaClient } = require("@prisma/client");
 
-const app = express()
-app.listen(5555, () => console.log('Rodando na porta 5555'))
-app.use(express.json())
-app.use(cors())
+const express = require("express");
+const cors = require("cors");
 
-// let users = [{
-//   id: 1,
-//   name: "Jakeliny Gracielly",
-//   avatar: "https://avatars.githubusercontent.com/u/17316392?v=4",
-//   city: "São Paulo"
-// }]
+const app = express();
+app.listen(5555, () => console.log("Rodando na porta 5555"));
+app.use(express.json());
+app.use(cors());
 
-let UsersOfCard = [];
+const prisma = new PrismaClient();
+// let UsersOfCard = [];
 
-app.get('/users', (req, res) => {
-  console.log("REPSOTA DO GET:",UsersOfCard);
-} )
-app.post('/usuarios', (req, res) => {
-  const {name, email, telefone} = req.body
-  const response = {
+// app.get('/users', (req, res) => {
+//   console.log("REPSOTA DO GET:",UsersOfCard);
+// } );
+// app.post('/usuarios', (req, res) => {
+//   const {name, email, telefone} = req.body
+//   const response = {
+//       name,
+//       email,
+//       telefone
+//   };
+//   UsersOfCard.push(response);
+//   console.log(UsersOfCard);
+//   console.log("Respoat do POST:",response);
+// } );
+// app.post('/new-user', (req, res) => {
+//   const { name, process, hour, day, shift } = req.body;
+//   const resp = {
+//     name,
+//     process,
+//     hour,
+//     day,
+//     shift
+//   };
+//   console.log("Resposta do post:", resp);
+// });
+
+app.post("/create-user", async (req, res) => {
+  const { name, email, senha } = req.body;
+
+  const resp = await prisma.usuarios.create({
+    data: {
       name,
       email,
-      telefone
-  };
-  UsersOfCard.push(response);
-  console.log(UsersOfCard);
-  console.log("Respoat do POST:",response);  
-} )
+      senha,
+    },
+  });
+  console.log(resp);
+  return res.status(200).json(resp);
+});
+app.patch("/update-user", async (req, res) => {
+  const { id, name, email, senha } = req.body;
+
+  const resp = await prisma.usuarios.update({
+    where: { id: parseInt(id) },
+    data: {
+      name,
+      email,
+      senha,
+    },
+  });
+  console.log(resp);
+  return res.status(200).json(resp);
+});
+app.delete("/delete-user", async (req, res) => {
+  const { id } = req.body;
+
+  const resp = await prisma.usuarios.delete({
+    where: { id: parseInt(id) },
+  });
+  console.log(resp);
+  return res.status(200).json(resp);
+});
